bd5630637c652951265aa5cd91fd8939295885be
Robert Gambee <robertgambee@gmail.com>
2022-12-31 18:00:20 -0500

    Import django.forms module instead of classes
    I'm only using each class once, so having to type `forms.` before each
    one isn't annoying.

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,30 +1,25 @@
+from django import forms
 from django.db.models import Count
-from django.forms import (
-    FloatField,
-    ModelChoiceField,
-    ModelForm,
-    ModelMultipleChoiceField,
-)
 
 from tracker.models import Entry, Tag
 
 
-class EntryForm(ModelForm):  # type: ignore[type-arg]
+class EntryForm(forms.ModelForm):  # type: ignore[type-arg]
     """A form for creating or editing an Entry
 
     Most validations are derived from the model. On top of that, amount is required to
     be non-negative.
     """
 
-    amount = FloatField(min_value=0.0)
+    amount = forms.FloatField(min_value=0.0)
     # Sort the choices for the category and tags by the number of matching Entries,
     # descending so the most common appear fist.
-    category = ModelChoiceField(
+    category = forms.ModelChoiceField(
         queryset=Tag.objects.exclude(name="")
         .annotate(num_entries=Count("entries_in_category"))
         .order_by("-num_entries")
     )
-    tags = ModelMultipleChoiceField(
+    tags = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.exclude(name="")
         .annotate(num_entries=Count("tagged_entries"))
         .order_by("-num_entries"),
