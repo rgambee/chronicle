d1aafccdc128d3efcb1639d7094beb9b3566289a
Robert Gambee <robertgambee@gmail.com>
2023-01-01 14:21:11 -0500

    Use autocomplete text box for category
    Advantages for the Select widget this field used previously:
    * Mouse friendly
    * Can see all known choices
    
    Advantages for the new Autocomplete widget:
    * Keyboard friendly
    * Not overwhelming when there are many options
    * Can enter a value that's not one of the provided choices
         * This is a disadvantage in other circumstances, but here it's
           useful when the user wants to add a new category.

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,9 +1,45 @@
+import logging
+import uuid
 from typing import Any
 
 from django import forms
+from django.forms.widgets import ChoiceWidget
 from django.utils import timezone
 
 from tracker.models import Entry, Tag
+
+
+class AutocompleteWidget(ChoiceWidget):
+    """A text box with autocompletion options"""
+
+    input_type = "text"
+    template_name = "tracker/widgets/autocomplete.html"
+    option_template_name = "django/forms/widgets/select_option.html"
+    add_id_index = False
+    checked_attribute = {}
+    option_inherits_attrs = False
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        if "list" not in self.attrs:
+            # This needs to be unique. UUID version 1 uses the system clock. Even if two
+            # instances of this class are created very close in time, they'll have
+            # different timestamps.
+            self.attrs["list"] = str(uuid.uuid1().time_low)
+
+    def format_value(self, value: Any) -> str:  # type: ignore[override]
+        # ChoiceWidget.format_value() always returns a list for some reason. Perhaps
+        # it's because it needs to support multiple ones being selected? That's not
+        # necessary in this case. And returning a list causes the HTML value to be
+        # rendered as `value="['myvalue']"` instead of `value="myvalue"`. Therefore, we
+        # return the first element of the list.
+        values = super().format_value(value)
+        if len(values) < 1:
+            logging.warning("Received no values to format")
+            return ""
+        if len(values) > 1:
+            logging.warning("Received multiple values to format: %s", values)
+        return values[0]
 
 
 class EntryForm(forms.ModelForm):  # type: ignore[type-arg]
@@ -18,7 +54,11 @@ class EntryForm(forms.ModelForm):  # type: ignore[type-arg]
         initial=timezone.localdate,
         widget=forms.DateInput(attrs={"type": "date"}),
     )
-    category = forms.ModelChoiceField(queryset=None, empty_label=None)
+    category = forms.ModelChoiceField(
+        queryset=None,
+        empty_label=None,
+        widget=AutocompleteWidget(attrs={"list": "id_category_list"}),
+    )
     tags = forms.ModelMultipleChoiceField(queryset=None, required=False)
 
     class Meta:
