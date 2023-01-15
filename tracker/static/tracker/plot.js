42f42f9334d4b2e0f3b8c9275b13c26958d9dc76
Robert Gambee <robertgambee@gmail.com>
2023-01-15 10:34:37 -0500

    Show daily total as line

--- tracker/static/tracker/plot.js
+++ tracker/static/tracker/plot.js
@@ -1,13 +1,27 @@
 const chart = echarts.init(document.getElementById("main"));
 const data = JSON.parse(document.getElementById("my-data").textContent);
 const series = [];
+const dateTotals = new Map();
+
 for (const [cat, dateObj] of Object.entries(data)) {
     series.push({
         name: cat,
         type: "bar",
         data: Object.entries(dateObj),
     });
+    for (const [date, amount] of Object.entries(dateObj)) {
+        if (!dateTotals.has(date)) {
+            dateTotals.set(date, 0);
+        }
+        dateTotals.set(date, dateTotals.get(date) + amount);
+    }
 }
+
+series.push({
+    name: "Daily Total",
+    type: "line",
+    data: Array.from(dateTotals),
+});
 
 const option = {
     title: {
