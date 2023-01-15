41c965ccca43476dd5a4988df02ae479b335f253
Robert Gambee <robertgambee@gmail.com>
2023-01-15 11:08:29 -0500

    Move bar chart into its own function
    This is in preparation of new charts I'll be adding soon. Having
    separate functions for them helps organize the code.

--- tracker/static/tracker/plot.js
+++ tracker/static/tracker/plot.js
@@ -1,43 +1,47 @@
-const chart = echarts.init(document.getElementById("main"));
-const data = JSON.parse(document.getElementById("my-data").textContent);
-const series = [];
-const dateTotals = new Map();
-
-for (const [cat, dateObj] of Object.entries(data)) {
-    series.push({
-        name: cat,
-        type: "bar",
-        data: Object.entries(dateObj),
-    });
-    for (const [date, amount] of Object.entries(dateObj)) {
-        if (!dateTotals.has(date)) {
-            dateTotals.set(date, 0);
+function configureBarChart(chart, data) {
+    const series = [];
+    const dateTotals = new Map();
+    for (const [cat, dateObj] of Object.entries(data)) {
+        series.push({
+            name: cat,
+            type: "bar",
+            data: Object.entries(dateObj),
+        });
+        for (const [date, amount] of Object.entries(dateObj)) {
+            if (!dateTotals.has(date)) {
+                dateTotals.set(date, 0);
+            }
+            dateTotals.set(date, dateTotals.get(date) + amount);
         }
-        dateTotals.set(date, dateTotals.get(date) + amount);
     }
+
+    series.push({
+        name: "Daily Total",
+        type: "line",
+        data: Array.from(dateTotals),
+    });
+
+    const option = {
+        title: {
+            text: "Amount by Date"
+        },
+        tooltip: {},
+        legend: {},
+        xAxis: {
+            type: "time",
+            name: "Date",
+        },
+        yAxis: {
+            type: "value",
+            name: "Amount",
+        },
+        series: series,
+    };
+
+    chart.setOption(option);
 }
 
-series.push({
-    name: "Daily Total",
-    type: "line",
-    data: Array.from(dateTotals),
-});
+const chart = echarts.init(document.getElementById("main"));
+const data = JSON.parse(document.getElementById("my-data").textContent);
 
-const option = {
-    title: {
-        text: "Amount by Date"
-    },
-    tooltip: {},
-    legend: {},
-    xAxis: {
-        type: "time",
-        name: "Date",
-    },
-    yAxis: {
-        type: "value",
-        name: "Amount",
-    },
-    series: series,
-};
-
-chart.setOption(option);
+configureBarChart(chart, data);
