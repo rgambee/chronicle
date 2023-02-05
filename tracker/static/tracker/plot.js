faa0ae3b0256f885e711951bf5ee9ed3b5c34f2e
Robert Gambee <robertgambee@gmail.com>
2023-02-05 11:24:47 -0500

    Show amount for each category in heatmap tooltip

--- tracker/static/tracker/plot.js
+++ tracker/static/tracker/plot.js
@@ -76,22 +76,40 @@ function configurePieChart(chart, data) {
     chart.setOption(option);
 }
 
-function configureCalendarHeatmap(chart, data) {
-    const dateTotals = new Map();
-    for (const dateObj of Object.values(data)) {
-        for (const [date, amount] of Object.entries(dateObj)) {
-            if (!dateTotals.has(date)) {
-                dateTotals.set(date, 0);
+function configureCalendarHeatmap(chart, dataByCategory) {
+    // dataByCategory maps category -> date -> amount.
+    // Rearrange it to be date -> category -> amount.
+    const dataByDate = new Map();
+    const totalKey = "_total";
+    for (const [category, dateToAmount] of Object.entries(dataByCategory)) {
+        for (const [date, amount] of Object.entries(dateToAmount)) {
+            if (!dataByDate.has(date)) {
+                dataByDate.set(date, new Map());
             }
-            dateTotals.set(date, dateTotals.get(date) + amount);
+            const categoryToAmount = dataByDate.get(date);
+            if (!categoryToAmount.has(category)) {
+                categoryToAmount.set(category, 0);
+            }
+            categoryToAmount.set(category, categoryToAmount.get(category) + amount);
+            // Also sum the total amount for this date
+            if (!categoryToAmount.has(totalKey)) {
+                categoryToAmount.set(totalKey, 0);
+            }
+            categoryToAmount.set(totalKey, categoryToAmount.get(totalKey) + amount);
         }
     }
-    const dataArray = Array.from(dateTotals).sort();
+
+    // Convert map to array of [date, totalAmount] pairs for plotting
+    const dateTotals = Array.from(
+        dataByDate,
+        ([date, categoryToAmount]) => [date, categoryToAmount.get(totalKey)],
+    ).sort();
+
     let start = new Date();
     let end = new Date();
-    if (dataArray.length > 0) {
-        start = new Date(dataArray.at(0).at(0));
-        end = new Date(dataArray.at(-1).at(0));
+    if (dateTotals.length > 0) {
+        start = new Date(dateTotals.at(0).at(0));
+        end = new Date(dateTotals.at(-1).at(0));
     }
     // Round to beginning of week (here assumed to start on Sunday)
     start.setDate(start.getDate() - start.getDay());
@@ -105,12 +123,9 @@ function configureCalendarHeatmap(chart, data) {
 
     const option = {
         tooltip: {
-            formatter: (params) => {
-                const [date, amount] = params.data;
-                return [
-                    "Amount: ",
-                    amount.toString(),
-                    "<br />",
+            formatter: params => {
+                const date = params.data[0];
+                const tooltipInfo = [[
                     new Date(date).toLocaleDateString(
                         undefined,
                         {
@@ -118,7 +133,18 @@ function configureCalendarHeatmap(chart, data) {
                             day: "numeric",
                         },
                     ),
-                ].join("");
+                ]];
+                if (dataByDate.has(date)) {
+                    dataByDate.get(date).forEach(
+                        (amount, category) => {
+                            if (category !== totalKey) {
+                                tooltipInfo.push([category, ": ", amount.toString()]);
+                            }
+                        },
+                    );
+                }
+                // Join each line with "", then join all the lines with breaks
+                return tooltipInfo.map(line => line.join("")).join("<br/>");
             },
         },
         calendar: {
@@ -127,12 +153,12 @@ function configureCalendarHeatmap(chart, data) {
         visualMap: {
             show: false,
             min: 0,
-            max: Math.max(...dateTotals.values()),
+            max: Math.max(...dateTotals.map(dateAndTotal => dateAndTotal[1])),
         },
         series: {
             type: "heatmap",
             coordinateSystem: "calendar",
-            data: dataArray,
+            data: dateTotals,
             label: {
                 show: true,
                 formatter: params => params.data[1].toString(),
