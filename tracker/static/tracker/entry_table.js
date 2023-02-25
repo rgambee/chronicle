5d11b7dbf9618aeed90388adfad8eed12cfc63cf
Robert Gambee <robertgambee@gmail.com>
2023-02-25 13:22:57 -0500

    Hide less important columns when window is narrow

--- tracker/static/tracker/entry_table.js
+++ tracker/static/tracker/entry_table.js
@@ -1,28 +1,36 @@
 new Tabulator(
     "#entry-table",
     {
+        responsiveLayout: "hide",
         columns: [
             {
                 title: "Date",
+                responsive: 10,
             },
             {
                 title: "Amount",
                 sorter: "number",
+                responsive: 10,
             },
             {
                 title: "Category",
+                responsive: 20,
             },
             {
                 title: "Tags",
+                responsive: 30,
             },
             {
                 title: "Comment",
+                responsive: 40,
             },
             {
                 title: "Edit",
+                responsive: 50,
             },
             {
                 title: "Delete",
+                responsive: 50,
             },
         ],
     },
