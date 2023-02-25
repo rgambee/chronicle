947bff150510be811a30624898a4dfd5d3d876f5
Robert Gambee <robertgambee@gmail.com>
2023-02-25 17:00:29 -0500

    Filter entries by date range

--- tracker/static/tracker/entry_table.js
+++ tracker/static/tracker/entry_table.js
@@ -1,3 +1,73 @@
+// Custom date filtering based on this answer by Oli Folkerd:
+// https://stackoverflow.com/a/64414478
+
+function rangeEditor(cell, onRendered, success, cancel) {
+    const startInput = document.createElement("input");
+    startInput.setAttribute("type", "date");
+    startInput.className = "form-control";
+    const endInput = startInput.cloneNode();
+
+    const container = document.createElement("span");
+    container.appendChild(startInput);
+    container.appendChild(endInput);
+
+    function buildRange() {
+        let start;
+        let end;
+        if (startInput.value) {
+            start = new Date(startInput.value);
+        }
+        if (endInput.value) {
+            end = new Date(endInput.value);
+        }
+        success(
+            {
+                start: start,
+                end: end,
+            },
+        );
+    }
+
+    function onKeypress(event) {
+        if (event.key === "Enter") {
+            buildRange();
+        }
+        else if (
+            event.key === "Esc" || event.key === "Backspace" || event.key === "Delete"
+        ) {
+            cancel();
+        }
+    }
+
+    startInput.addEventListener("change", buildRange);
+    startInput.addEventListener("blur", buildRange);
+    startInput.addEventListener("keydown", onKeypress);
+    endInput.addEventListener("change", buildRange);
+    endInput.addEventListener("blur", buildRange);
+    endInput.addEventListener("keydown", onKeypress);
+
+    return container;
+}
+
+function rangeFilter(dateRange, dateValue) {
+    if (!dateValue) {
+        // Include empty dates by default
+        return true;
+    }
+    dateValue = new Date(dateValue);
+    if (dateRange.start) {
+        if (dateRange.end) {
+            return dateValue >= dateRange.start && dateValue <= dateRange.end;
+        } else {
+            return dateValue >= dateRange.start;
+        }
+    } else if (dateRange.end) {
+        return dateValue <= dateRange.end;
+    }
+    // If neither side of range is defined, return true so all rows are included
+    return true;
+}
+
 new Tabulator(
     "#entry-table",
     {
@@ -12,6 +82,10 @@ new Tabulator(
                     inputFormat:"yyyy-MM-dd",
                     outputFormat:"DD",
                 },
+                width: 175,
+                headerFilter: rangeEditor,
+                headerFilterFunc: rangeFilter,
+                headerFilterLiveFilter: false,
             },
             {
                 title: "Amount",
