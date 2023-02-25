4d7095a76572ed5ef8cf43af74592e6e7cb3805c
Robert Gambee <robertgambee@gmail.com>
2023-02-25 13:11:36 -0500

    Use Tabulator for entry table
    After more configuration, this will allow sorting, filtering and
    editing.

---
+++ tracker/static/tracker/entry_table.js
@@ -0,0 +1,29 @@
+new Tabulator(
+    "#entry-table",
+    {
+        columns: [
+            {
+                title: "Date",
+            },
+            {
+                title: "Amount",
+                sorter: "number",
+            },
+            {
+                title: "Category",
+            },
+            {
+                title: "Tags",
+            },
+            {
+                title: "Comment",
+            },
+            {
+                title: "Edit",
+            },
+            {
+                title: "Delete",
+            },
+        ],
+    },
+);
