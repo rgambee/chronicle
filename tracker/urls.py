8ee2c72b3d5602ee43e8522ffb36baf19138a028
Robert Gambee <robertgambee@gmail.com>
2023-03-03 20:58:12 -0500

    Reformat URL pattern list to be more consistent
    Not all entries were long enough to exceed the line length, but I think
    it looks better for them all to be formatted the same way.

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,9 +5,21 @@ from django.urls import path
 from tracker import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("entry/<int:pk>/", views.EntryDetailView.as_view(), name="entry"),
-    path("entries/", views.EntryListAndCreate.as_view(), name="entries"),
+    path(
+        "",
+        views.index,
+        name="index",
+    ),
+    path(
+        "entry/<int:pk>/",
+        views.EntryDetailView.as_view(),
+        name="entry",
+    ),
+    path(
+        "entries/",
+        views.EntryListAndCreate.as_view(),
+        name="entries",
+    ),
     path(
         "entries/<str:category>/<int:amount><str:unit>/",
         views.EntryListAndCreate.as_view(),
@@ -23,9 +35,32 @@ urlpatterns = [
         views.EntryListAndCreate.as_view(),
         name="entries-in-category",
     ),
-    path("edit/<int:pk>/", views.EntryEdit.as_view(), name="edit"),
-    path("delete/<int:pk>/", views.EntryDelete.as_view(), name="delete"),
-    path("charts/", views.ChartView.as_view(), name="charts"),
-    path("charts/<int:amount><str:unit>/", views.ChartView.as_view(),name="charts-recent"),
-    path("preferences/", views.PreferencesEdit.as_view(), name="preferences"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path(
+        "edit/<int:pk>/",
+        views.EntryEdit.as_view(),
+        name="edit",
+    ),
+    path(
+        "delete/<int:pk>/",
+        views.EntryDelete.as_view(),
+        name="delete",
+    ),
+    path(
+        "charts/",
+        views.ChartView.as_view(),
+        name="charts",
+    ),
+    path(
+        "charts/<int:amount><str:unit>/",
+        views.ChartView.as_view(),
+        name="charts-recent",
+    ),
+    path(
+        "preferences/",
+        views.PreferencesEdit.as_view(),
+        name="preferences",
+    ),
+] + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT,
+)
