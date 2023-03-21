df6490cd6f13f3e4352bf82534cfb40108be6d41
Robert Gambee <robertgambee@gmail.com>
2023-03-21 10:27:48 -0400

    Move view utility functions to new module
    The views.py module was getting long and disorganized. Now it only
    contains classes and functions for handling HTTP requests (in line
    with Django's meaning of "view").
    
    The new view_utils.py module has assorted helper functions for selecting
    and formatting entries. These helpers are used by views.py to send data
    to the client.

---
+++ tracker/tests/test_view_utils.py
@@ -0,0 +1,310 @@
+from datetime import datetime, timedelta, timezone
+from unittest.mock import MagicMock, patch
+
+from django.test import TestCase
+from django.utils.timezone import get_current_timezone, make_aware, override
+
+from tracker.models import Entry, Tag
+from tracker.tests.utils import TrackerTestCase
+from tracker.view_utils import (
+    aggregate_entries,
+    format_datetime_ecma,
+    get_recent_entries,
+    subtract_timedelta,
+)
+
+
+class TestRecentEntries(TrackerTestCase):
+    tags = (Tag("t"),)
+    entries = (
+        Entry(amount=1.0, date=make_aware(datetime(2000, 3, 1)), category=tags[0]),
+        Entry(amount=2.0, date=make_aware(datetime(2000, 3, 1)), category=tags[0]),
+        Entry(amount=3.0, date=make_aware(datetime(2000, 2, 25)), category=tags[0]),
+        Entry(amount=4.0, date=make_aware(datetime(2000, 2, 1)), category=tags[0]),
+        Entry(amount=5.0, date=make_aware(datetime(2000, 1, 1)), category=tags[0]),
+    )
+
+    def test_all_entries(self) -> None:
+        """If no optional arguments are given, all entries should be returned"""
+        self.assertQuerysetEqual(
+            get_recent_entries(Entry.objects.all()),
+            self.entries,
+        )
+
+    def test_one_year(self) -> None:
+        """Should return all entries within the previous year"""
+        self.assertQuerysetEqual(
+            get_recent_entries(
+                queryset=Entry.objects.all(),
+                amount=1,
+                unit="years",
+                end=make_aware(datetime(2000, 3, 1)),
+            ),
+            self.entries,
+        )
+
+    def test_one_month(self) -> None:
+        """Should return all entries within the previous month"""
+        self.assertQuerysetEqual(
+            get_recent_entries(
+                queryset=Entry.objects.all(),
+                amount=1,
+                unit="months",
+                end=make_aware(datetime(2000, 3, 1)),
+            ),
+            self.entries[:-1],
+        )
+
+    def test_one_week(self) -> None:
+        """Should return all entries within the previous week"""
+        self.assertQuerysetEqual(
+            get_recent_entries(
+                queryset=Entry.objects.all(),
+                amount=1,
+                unit="weeks",
+                end=make_aware(datetime(2000, 3, 1)),
+            ),
+            self.entries[:-2],
+        )
+
+    def test_one_day(self) -> None:
+        """Should return all entries within the previous day"""
+        self.assertQuerysetEqual(
+            get_recent_entries(
+                queryset=Entry.objects.all(),
+                amount=1,
+                unit="days",
+                end=make_aware(datetime(2000, 3, 1)),
+            ),
+            self.entries[:-3],
+        )
+
+    def test_order(self) -> None:
+        """Order of input queryset should be preserved"""
+        self.assertQuerysetEqual(
+            get_recent_entries(
+                queryset=Entry.objects.order_by("-amount"),
+                amount=1,
+                unit="years",
+                end=make_aware(datetime(2000, 3, 1)),
+            ),
+            self.entries[::-1],
+        )
+
+    @patch("tracker.view_utils.timezone.now")
+    def test_default_end(self, now_mock: MagicMock) -> None:
+        """End date should default to today"""
+        now_mock.return_value = make_aware(datetime(2000, 2, 25))
+        self.assertQuerysetEqual(
+            get_recent_entries(
+                queryset=Entry.objects.all(),
+                amount=4,
+                unit="weeks",
+            ),
+            self.entries[2:-1],
+        )
+
+
+class TestSubtractTimedelta(TestCase):
+    def test_years(self) -> None:
+        """Subtracting years should leave other fields untouched"""
+        end = datetime(2000, 3, 21, 12, 34, 56)
+        start = subtract_timedelta(end, 0, "years")
+        self.assertEqual(start, end)
+        start = subtract_timedelta(end, 1, "years")
+        self.assertEqual(start, datetime(1999, 3, 21, 12, 34, 56))
+        start = subtract_timedelta(end, 100, "years")
+        self.assertEqual(start, datetime(1900, 3, 21, 12, 34, 56))
+
+    def test_months(self) -> None:
+        """Subtracting months should decrement year when appropriate"""
+        end = datetime(2000, 3, 21, 12, 34, 56)
+        start = subtract_timedelta(end, 0, "months")
+        self.assertEqual(start, end)
+        start = subtract_timedelta(end, 1, "months")
+        self.assertEqual(start, datetime(2000, 2, 21, 12, 34, 56))
+        start = subtract_timedelta(end, 3, "months")
+        self.assertEqual(start, datetime(1999, 12, 21, 12, 34, 56))
+        start = subtract_timedelta(end, 12, "months")
+        self.assertEqual(start, datetime(1999, 3, 21, 12, 34, 56))
+
+    def test_weeks(self) -> None:
+        """Subtracting weeks should follow the expected rules"""
+        end = datetime(2000, 3, 21, 12, 34, 56)
+        start = subtract_timedelta(end, 0, "weeks")
+        self.assertEqual(start, end)
+        start = subtract_timedelta(end, 1, "weeks")
+        self.assertEqual(start, datetime(2000, 3, 14, 12, 34, 56))
+        start = subtract_timedelta(end, 3, "weeks")
+        self.assertEqual(start, datetime(2000, 2, 29, 12, 34, 56))
+        start = subtract_timedelta(end, 8, "weeks")
+        self.assertEqual(start, datetime(2000, 1, 25, 12, 34, 56))
+
+    def test_days(self) -> None:
+        """Subtracting days should follow the expected rules"""
+        end = datetime(2000, 3, 21, 12, 34, 56)
+        start = subtract_timedelta(end, 0, "days")
+        self.assertEqual(start, end)
+        start = subtract_timedelta(end, 1, "days")
+        self.assertEqual(start, datetime(2000, 3, 20, 12, 34, 56))
+        start = subtract_timedelta(end, 21, "days")
+        self.assertEqual(start, datetime(2000, 2, 29, 12, 34, 56))
+
+    def test_negative_amount(self) -> None:
+        """A negative amount should raise a ValueError"""
+        end = datetime(2000, 3, 21, 12, 34, 56)
+        with self.assertRaises(ValueError):
+            subtract_timedelta(end, -1, "days")
+
+    def test_unrecognized_unit(self) -> None:
+        """Unrecognized units should raise a TypeError
+
+        A ValueError feels more appropriate to me, but the datetime.timedelta() raises a
+        TypeError in this situation.
+        """
+        end = datetime(2000, 3, 21, 12, 34, 56)
+        with self.assertRaises(TypeError):
+            subtract_timedelta(end, 1, "fortnights")
+
+    def test_day_out_of_range(self) -> None:
+        """Invalid dates (like February 30th) should shift forward to next valid one"""
+        end = datetime(2000, 3, 31, 12, 34, 56)
+        start = subtract_timedelta(end, 1, "months")
+        self.assertEqual(start, end.replace(day=1))
+        start = subtract_timedelta(end, 2, "months")
+        self.assertEqual(start, end.replace(month=1))
+        start = subtract_timedelta(end, 3, "months")
+        self.assertEqual(start, datetime(1999, 12, 31, 12, 34, 56))
+        start = subtract_timedelta(end, 4, "months")
+        self.assertEqual(start, datetime(1999, 12, 1, 12, 34, 56))
+        # Test subtracting from a leap year
+        end = datetime(2000, 2, 29, 12, 34, 56)
+        start = subtract_timedelta(end, 1, "years")
+        self.assertEqual(start, datetime(1999, 3, 1, 12, 34, 56))
+
+
+class TestEntryAggregation(TrackerTestCase):
+    timezone_for_testing = timezone.utc
+    tags = (Tag("red"), Tag("blue"))
+    entries = (
+        Entry(
+            amount=1.0,
+            date=datetime(2000, 3, 1, tzinfo=timezone_for_testing),
+            category=tags[0],
+        ),
+        Entry(
+            amount=2.0,
+            date=datetime(2000, 2, 1, tzinfo=timezone_for_testing),
+            category=tags[1],
+        ),
+        Entry(
+            amount=3.0,
+            date=datetime(2000, 1, 1, tzinfo=timezone_for_testing),
+            category=tags[0],
+        ),
+    )
+
+    def test_empty_queryset(self) -> None:
+        """An empty queryset should yield an empty dict"""
+        self.assertEqual(aggregate_entries(Entry.objects.none()), {})
+
+    @override(timezone_for_testing)
+    def test_one_category(self) -> None:
+        """One category should yield a dict with one item"""
+        aggregated = aggregate_entries(Entry.objects.filter(category_id=self.tags[0]))
+        self.assertEqual(
+            aggregated,
+            {
+                self.tags[0].name: {
+                    format_datetime_ecma(self.entries[0].date): self.entries[0].amount,
+                    format_datetime_ecma(self.entries[2].date): self.entries[2].amount,
+                },
+            },
+        )
+
+        aggregated = aggregate_entries(Entry.objects.filter(category_id=self.tags[1]))
+        self.assertEqual(
+            aggregated,
+            {
+                self.tags[1].name: {
+                    format_datetime_ecma(self.entries[1].date): self.entries[1].amount,
+                },
+            },
+        )
+
+    @override(timezone_for_testing)
+    def test_multiple_categories(self) -> None:
+        """Multiple categories should yield a dict with multiple items"""
+        aggregated = aggregate_entries(Entry.objects.all())
+        self.assertEqual(
+            aggregated,
+            {
+                self.tags[0].name: {
+                    format_datetime_ecma(self.entries[0].date): self.entries[0].amount,
+                    format_datetime_ecma(self.entries[2].date): self.entries[2].amount,
+                },
+                self.tags[1].name: {
+                    format_datetime_ecma(self.entries[1].date): self.entries[1].amount,
+                },
+            },
+        )
+
+    @override(timezone_for_testing)
+    def test_prefiltered_queryset(self) -> None:
+        """Aggregation only include entries in the given queryset"""
+        queryset = Entry.objects.filter(category_id=self.tags[0], amount__gte=2.0)
+        aggregated = aggregate_entries(queryset)
+        self.assertEqual(
+            aggregated,
+            {
+                self.tags[0].name: {
+                    format_datetime_ecma(self.entries[2].date): self.entries[2].amount,
+                },
+            },
+        )
+
+    def test_timezone(self) -> None:
+        """Dates should be converted to current timezone before aggregation"""
+        queryset = Entry.objects.filter(category_id=self.tags[1])
+
+        for offset_hours in (-23, -1, 0, 1, 23):
+            with override(timezone(timedelta(hours=offset_hours))):
+                with self.subTest(timezone=get_current_timezone()):
+                    aggregated = aggregate_entries(queryset)
+                    expected_datetime = format_datetime_ecma(
+                        # pylint thinks Entry.date has type DateTimeField, when the
+                        # actual type is datetime, making it complain about using the
+                        # astimezone() method. This is understandable since Django is
+                        # overriding the class attribute with the instance attribute.
+                        # pylint: disable-next=no-member
+                        self.entries[1]
+                        .date.astimezone(get_current_timezone())
+                        .replace(hour=0, minute=0, second=0, microsecond=0)
+                    )
+                    self.assertEqual(
+                        aggregated,
+                        {
+                            self.tags[1].name: {
+                                expected_datetime: self.entries[1].amount,
+                            },
+                        },
+                    )
+
+
+class TestDatetimeFormat(TestCase):
+    def test_format(self) -> None:
+        """Datetimes should be converted to ISO-8601 format with timezone info"""
+        sample_dt = datetime(
+            year=2000,
+            month=1,
+            day=2,
+            hour=12,
+            minute=34,
+            second=56,
+            microsecond=987654,
+            tzinfo=timezone(timedelta(hours=12, minutes=34)),
+        )
+        self.assertEqual(
+            format_datetime_ecma(sample_dt),
+            "2000-01-02T12:34:56.987+12:34",
+        )
--- tracker/views.py
+++ tracker/views.py
@@ -1,15 +1,12 @@
 import logging
-from datetime import datetime, timedelta
 from typing import Any, Optional
 
 from django.contrib.messages.views import SuccessMessageMixin
-from django.db.models import Sum
 from django.db.models.query import QuerySet
 from django.forms import Form
-from django.http import Http404, HttpRequest, HttpResponse, HttpResponseBase
+from django.http import HttpRequest, HttpResponse, HttpResponseBase
 from django.shortcuts import redirect
 from django.urls import reverse, reverse_lazy
-from django.utils import timezone
 from django.views import View
 from django.views.generic import DeleteView, DetailView, FormView, ListView
 from django.views.generic.base import ContextMixin
@@ -17,6 +14,7 @@ from django.views.generic.edit import CreateView, UpdateView
 
 from tracker.forms import EntryForm, PreferencesForm
 from tracker.models import Entry
+from tracker.view_utils import aggregate_entries, get_recent_entries
 
 
 def index(_: HttpRequest) -> HttpResponse:
@@ -66,13 +64,9 @@ class EntryListView(NavBarLinksMixin, ListView):  # type: ignore[type-arg]
         queryset = Entry.objects.all()
         if "category" in self.kwargs:
             queryset = queryset.filter(category=self.kwargs["category"])
-        try:
-            queryset = get_recent_entries(
-                queryset, self.kwargs.get("amount"), self.kwargs.get("unit")
-            )
-        except (TypeError, ValueError) as err:
-            raise Http404("Invalid date range") from err
-        return queryset
+        return get_recent_entries(
+            queryset, self.kwargs.get("amount"), self.kwargs.get("unit")
+        )
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         return super().get_context_data(
@@ -201,121 +195,3 @@ class PreferencesEdit(NavBarLinksMixin, FormView):  # type: ignore[type-arg]
     def form_valid(self, form: Form) -> HttpResponse:
         logging.info(form.cleaned_data)
         return super().form_valid(form)
-
-
-def get_recent_entries(
-    queryset: QuerySet[Entry],
-    amount: Optional[int] = None,
-    unit: Optional[str] = None,
-    end: Optional[datetime] = None,
-) -> QuerySet[Entry]:
-    """Filter the given queryset to only contain recent entries
-
-    See the documentation for subtract_timedelta() for the requirements regarding the
-    `amount` and `unit` arguments.
-
-    `end` defaults to now.
-    """
-    if amount is not None and unit is not None:
-        if end is None:
-            end = timezone.now()
-        try:
-            start = subtract_timedelta(end, amount, unit)
-        except (TypeError, ValueError) as err:
-            raise Http404("Invalid date range") from err
-        queryset = queryset.filter(date__gte=start).filter(date__lte=end)
-    elif amount is not None or unit is not None:
-        logging.error("Invalid arguments: amount=%s, unit=%s", amount, unit)
-        raise TypeError("Must provide both amount and unit or neither")
-    return queryset
-
-
-def subtract_timedelta(end: datetime, amount: int, unit: str) -> datetime:
-    """Subtract the given amount of time from an end date
-
-    `amount` must be a non-negative integer.
-
-    `unit` may be one of the following:
-        "years"
-        "months"
-        "weeks"
-        "days"
-        "hours"
-        "minutes"
-        "seconds"
-    """
-    # Time spans of weeks through seconds are well-defined, and we can rely on the
-    # datetime library for the arithmetic. But years and months don't have fixed
-    # durations, so they require some extra logic. In this case, we want to decrement
-    # the year/month since that's what a person usually means by "one year/month ago".
-    if amount < 0:
-        raise ValueError("Time delta amount may not be negative")
-    if unit == "years":
-        year = end.year - amount
-        start = datetime_replace(end, year=year)
-    elif unit == "months":
-        month = (end.month - amount - 1) % 12 + 1
-        year = end.year + (end.month - amount - 1) // 12
-        start = datetime_replace(end, year=year, month=month)
-    else:
-        delta = timedelta(**{unit: amount})
-        start = end - delta
-    return start
-
-
-def datetime_replace(when: datetime, **kwargs: int) -> datetime:
-    """Replace fields of a datetime, rolling forward to avoid invalid dates"""
-    try:
-        # mypy complains that we might try to set the tzinfo field to an int, which
-        # would be a problem and is technically possible given signature of this
-        # function. Admittedly, the signature is incorrect (kwargs elements can have
-        # type int or Optional[tzinfo]), but annotating it correctly would be verbose
-        # and less clear.
-        result = when.replace(**kwargs)  # type: ignore[arg-type]
-    except ValueError as err:
-        if str(err) == "day is out of range for month":
-            # Round invalid dates (like February 30th) forward to next valid one
-            year = kwargs.get("year", when.year)
-            month = kwargs.get("month", when.month)
-            year += month // 12
-            month = month % 12 + 1
-            kwargs.pop("year", None)
-            kwargs.pop("month", None)
-            kwargs.pop("day", None)
-            result = when.replace(
-                year=year, month=month, day=1, **kwargs  # type: ignore[arg-type]
-            )
-        else:
-            raise
-    return result
-
-
-def aggregate_entries(queryset: QuerySet[Entry]) -> dict[str, dict[str, float]]:
-    """Group entries first by category, then by date"""
-    # Determine unique categories and dates
-    categories = queryset.order_by("category_id").values("category_id").distinct()
-    dates = queryset.datetimes(field_name="date", kind="day")
-    # For each (category, date) pair, sum the amounts of the corresponding entries
-    aggregated: dict[str, dict[str, float]] = {}
-    for cat in categories:
-        aggregated[cat["category_id"]] = {}
-        for day in dates:
-            total = queryset.filter(
-                category=cat["category_id"],
-                date__date=day.date(),
-            ).aggregate(Sum("amount"))
-            if total["amount__sum"]:
-                formatted_date = format_datetime_ecma(day)
-                aggregated[cat["category_id"]][formatted_date] = total["amount__sum"]
-    return aggregated
-
-
-def format_datetime_ecma(when: datetime) -> str:
-    """Format the given datetime according to the official ECMAScript spec
-
-    This aims to ensure maximum compatibility with different browsers.
-
-    The spec is defined here:
-    https://tc39.es/ecma262/#sec-date-time-string-format
-    """
-    return when.isoformat(timespec="milliseconds")
